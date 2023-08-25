499c55f3f3d53830ce153d90dab128bba3c1dce7
thiagomedinad21@gmail.com <thiagomedinad21@gmail.com>
2023-08-25 10:55:21 -0300

    test Student class implementations

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Thiago", "Medina");
-        Student s2 = new Student("Ryan", "Flores");
+        Student s1 = new Student();
 
         System.out.println(s1.getTuition());
         System.out.println(s1.getRegistrationCode());
-
-        System.out.println(s2.getTuition());
-        System.out.println(s2.getRegistrationCode());
+        System.out.println(s1.getTotalHours());
+        System.out.println(s1.getEntryDate());
     }
 }
